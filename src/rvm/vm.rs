51f4f575965f1affd08e4b9304505db2b4a95265
xermic <bigcyrill@hotmail.com>
2017-11-28 07:49:39 +0100

    working on run

--- src/rvm/vm.rs
+++ src/rvm/vm.rs
@@ -8,45 +8,91 @@ pub struct Context {
 }
 
 impl Context {
-	fn fetch(&mut self) -> Result<Instruction, Error> {
+	fn fetch(&mut self) -> Result<Instruction, VMError> {
 		let pointer_next = self.registers[RN as usize];
 		if let Some(instruction) = self.bytecode.get(pointer_next as usize) {
 			if let Some(next) = pointer_next.checked_add(1) {
 				self.registers[RN as usize] = next;
 			} else {
-				if (instruction & 0xF00) >> 8 == RN as u16 {
-					return Err(Error::VMContextFetchNextError)
+				if decode_target(instruction) == RN {
+					return Err(VMError::VMContextFetchNextError)
 				}
 			}
 			Ok(instruction.to_owned())
 		} else {
-			return Err(Error::VMContextFetchInvalidError)
+			return Err(VMError::VMContextFetchInvalidError)
 		}
+	}
+
+	fn step(&mut self) -> Result<Instruction, VMError> {
+		match self.fetch() {
+			Ok(instruction) => {
+				println!("fetched: 0x{:4x}", instruction);
+				match decode_opcode(&instruction) {
+					INT => {
+						match decode_target(&instruction) {
+							HLT => { println!("Debug: got HLT") },
+							SYS => { println!("Debug: got SYS") },
+							_ => { return Err(VMError::VMRunError) }
+						}
+					},
+					SET => {},
+					STO => {},
+					ADD => {},
+					SUB => {},
+					MUL => {},
+					DIV => {},
+					CHK => {},
+					CNS => {},
+					LPT => {},
+					LSH => {},
+					RSH => {},
+					AND => {},
+					BOR => {},
+					XOR => {},
+					_ => return Err(VMError::VMRunError)
+				}
+				return Ok(instruction)
+			},
+			Err(error) => return Err(error)
+		};
+
+		return Err(VMError::VMRunError)
 	}
 }
 
-pub fn run(bytecode: Bytecode) -> Result<Context, Error> {
+pub fn run(bytecode: Bytecode) -> Result<Context, VMError> {
 	let mut context = Context::default();	
 	context.bytecode = bytecode;
 
 	loop {
-		match context.fetch() {
-			Ok(instruction) => println!("fetched: 0x{:4x}", instruction),
-			Err(error) => { 
+		match context.step()	{
+			Ok(instruction) => println!("Step ok"),
+			Err(error) => {
 				let faulty_index = context.registers[RN as usize];
 				match error {
-					Error::VMContextFetchNextError => 
+					VMError::VMContextFetchNextError => 
 						println!("Error while fetching instruction at index 0x{:x}\n\t-> Hint: Programm too large?", faulty_index),
-					Error::VMContextFetchInvalidError =>
+					VMError::VMContextFetchInvalidError =>
 						println!("Error while fetching instruction at index 0x{:x}\n\t-> Hint: No instruction found at this index", faulty_index),
-					_ =>
-						println!("Error while fetching next instruction at index 0x{:2x}\n\t-> Hint: Check your code", faulty_index),
+					_ => println!("Runtime Error")
 				};
-				return Err(Error::VMRunError)
+				break
 			}
-		};
-			
+		}; 
 	}
 
 	Ok(Context::default())
+}
+
+fn decode_opcode(instruction: &Instruction) -> Rsize {
+	((instruction & 0xF000) >> 12) as Rsize
+}
+
+fn decode_target(instruction: &Instruction) -> Rsize {
+	((instruction & 0x0F00) >> 8) as Rsize
+}
+
+fn decode_value(instruction: &Instruction) -> Rsize {
+	((instruction & 0x00FF)) as Rsize
 }
